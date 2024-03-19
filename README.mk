1fa664115c302aa4ad1250e51387cd8915054cae
mahthviana <94473682+mahthviana@users.noreply.github.com>
2024-03-19 14:57:11 -0300

    Add files via upload

---
+++ README.mk
@@ -0,0 +1,7 @@
+# Atvidadde do curso Desenvolvimento FullStack da Infinity School
+
+Atividade da Aula 03 do curso de desnvolvimento full stack.
+
+Nessa atividade foram trabalhados o uso de **formulários e agrupamentos** com o objetivo de fazer uma tela simples de login com email e senha mais o uso do link de "Esqueceu sua senha". 
+
+Site oficial da escola: <https://infinityschool.com.br/>
